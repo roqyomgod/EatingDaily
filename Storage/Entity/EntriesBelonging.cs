06c0cbc25d4905f3fe384dc387e38caf4407f9b4
IlyaI <IlyaI@DESKTOP-6HGUK6R>
2021-05-17 22:59:28 +0300

    DataBase end

---
+++ Storage/Entity/EntriesBelonging.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EatingDaily.Storage.Entity
+{
+    public class EntriesBelonging
+    {
+        [Key]
+        public int BelongingID { get; set; }
+        public int DiaryID { get; set; }
+        public int Type { get; set; }
+        public int EntryID { get; set; }
+    }
+}
