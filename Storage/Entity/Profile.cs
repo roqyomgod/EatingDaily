c6e6475a66afa265ee36019da1af0859c649d019
IlyaI <IlyaI@DESKTOP-6HGUK6R>
2021-05-17 20:03:50 +0300

    DATABASE

---
+++ Storage/Entity/Profile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EatingDaily.Storage.Entity
+{
+    public class Profile
+    {
+		[Key]
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string LastName { get; set; }
+		public string Phone { get; set; }
+		public int Age { get; set; }
+		public int ImageID { get; set; }
+	}
+}
---
+++ Storage/Entity/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EatingDaily.Storage.Entity
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
