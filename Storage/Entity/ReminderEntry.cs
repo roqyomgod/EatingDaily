600ee38fa9bc68afbfca6ceda48982e819f0e4ce
Илья Чечеткин <emilwillond@gmail.com>
2021-05-17 21:48:44 +0300

    Reminder (tabletki)

---
+++ Storage/Entity/ReminderEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EatingDaily.Storage.Entity
+{
+    public class ReminderEntry : Entry
+    {
+        public DateTime Time { get; set; } = DateTime.Now;
+        public string Email { get; set; }
+    }
+}
---
+++ Storage/WorkContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EatingDaily.Storage.Entity;
+//using EatingDaily.Storage.StorgeEntity;
+
+namespace EatingDaily.Storage
+{
+    public class WorkContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Diet> Diets { get; set; }
+        public DbSet<Belonging> Belongings { get; set; }
+        public DbSet<EntriesBelonging> Entries { get; set; }
+        public DbSet<TextEntry> TextEntries { get; set; }
+        public DbSet<ReminderEntry> ReminderEntries { get; set; }
+        public DbSet<TimerEntry> TimerEntries { get; set; }
+        public DbSet<ChecklistEntry> ChecklistEntries { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<Source> SourceData { get; set; }
+        public DbSet<ImageEntry> Images { get; set; }
+
+        public WorkContext(DbContextOptions<WorkContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
