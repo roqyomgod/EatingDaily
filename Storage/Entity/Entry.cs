9201f003cc6231f0169dd9a511d3583042e749e2
IlyaI <IlyaI@DESKTOP-6HGUK6R>
2021-05-17 20:22:21 +0300

    DATABASE2

---
+++ Storage/Entity/Entry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EatingDaily.Storage.Entity
+{
+    public class Entry
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+    }
+}
