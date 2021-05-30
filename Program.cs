9a04f689ea4d477056706011436d719b60d7cfec
Илья Чечеткин <emilwillond@gmail.com>
2021-05-30 20:58:34 +0300

    reg/log

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+using EatingDaily.Storage;
+using EatingDaily.Storage.StorageEntity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,9 +16,24 @@ namespace EatingDaily
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
 
+                try
+                {
+                    var context = services.GetRequiredService<WorkContext>();
+                    AdminUser.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+            host.Run();
+        }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
