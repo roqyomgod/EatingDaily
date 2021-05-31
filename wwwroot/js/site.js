02504de05438ff57b2d87402ff5e61eed11dd9c0
Илья Чечеткин <emilwillond@gmail.com>
2021-06-01 01:03:47 +0300

    Ня-ня-ня-ня-ня-ня-ян-ян-ян-яня

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
-// Write your JavaScript code.
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
